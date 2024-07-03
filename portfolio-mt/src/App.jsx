0998b15085566acfec88829b8249cb6c23e757eb
Michael Torres <michael.46@live.it>
2024-07-03 13:58:54 +0200

    install font on project and setting as universal

--- portfolio-mt/src/App.jsx
+++ portfolio-mt/src/App.jsx
@@ -1,4 +1,6 @@
 import "./index.css"; // O il percorso corretto al tuo file CSS
-export default function App() {
-  return <h1 className="bg-red-500 text-3xl font-bold underline">world!</h1>;
-}
+const App = () => {
+  return <h1 className="bg-red-500 text-3xl text-center ">Michael Torres!</h1>;
+};
+
+export default App;
